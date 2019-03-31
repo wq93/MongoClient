e357839f8afc53eac09e71b6782a7df53b416a2e
wangqi <wangqi@chicv.com>
2019-03-31 13:43:34 +0800

    add mongoose connect but error

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 console.log('app running ...');
-
-
-const MongoClient = require('mongodb').MongoClient;
 const config = require('./config')
-const uri = `mongodb+srv://${config.MongoDB.username}:${config.MongoDB.password}@cluster0-w5bwk.mongodb.net/test?retryWrites=true`;
+const uri = `mongodb+srv://${config.MongoDB.username}:${config.MongoDB.password}@cluster0-w5bwk.mongodb.net`;
 
+
+/*
+* mongodb连接数据库法
+* */
+const MongoClient = require('mongodb').MongoClient;
 MongoClient.connect(uri, {useNewUrlParser: true}, (err, client) => {
   if (err) {
     console.log('connect error', err)
@@ -17,46 +19,18 @@ MongoClient.connect(uri, {useNewUrlParser: true}, (err, client) => {
   }
 });
 
-// let client;
-//
-// if (client && client.isConnected()) {
-//   console.log('DB CLIENT ALREADY CONNECTED')
-// } else  {
-//   client =  MongoClient.connect(uri, { useNewUrlParser: true })
-//   console.log('DB CLIENT RECONNECTED')
-// }
-
+/*
+* mongoose
+* */
 // const mongoose = require('mongoose');
-// const config = require('./config')
-// // const {Goods: GetGoods} = require('./models/Goods')
-// mongoose
-//   .connect(
-//     `mongodb+srv://${config.MongoDB.username}:${config.MongoDB.password}@cluster0-w5bwk.mongodb.net/test?retryWrites=true`,
-//     { useNewUrlParser: true }
-//   )
-//   .then(() => {
-//
+// const Schema = mongoose.Schema
+// mongoose.connect(uri, {useNewUrlParser: true})
+//   .then(db => {
 //     console.log("Connected to mongodb...")
-//     var Schema = mongoose.Schema
+//     var MyModel = mongoose.model('Goods', new Schema());
 //
-//     var produtSchema = new Schema({
-//       'uuid': {type: String}, // id
-//       'displayName': String, // 名称
-//       'type': String, // 类别 1:日用品 2:洗簌 3:出行
-//       'count': Number, // 数量
-//       'price': Number, // 价格
-//       'source': String, // 来源
-//       'remark': String // 备注
-//     })
-//     const Goods = mongoose.model('good', produtSchema);
-//
-//
-//     Goods.find(function (err,doc) {
-//      if(err) {
-//        console.log(err);
-//      }else{
-//        console.log('sucess:',doc);
-//      }
-//     })
+//     MyModel.find(function (error, result) {
+//       console.log(result);
+//     });
 //   })
 //   .catch(err => console.log(err));
